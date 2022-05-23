1649e2ab26696cb0962452055384b6c873dd289b
lhy <1580575248@qq.com>
2022-05-23 14:00:45 +0800

    lab2

---
+++ src/main/java/com/luohongyun/lab2/YourNameFilter.java
@@ -0,0 +1,25 @@
+package com.luohongyun.lab2;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class YourNameFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("YourNameFilter-->before chain");
+        filterChain.doFilter(servletRequest,servletResponse);
+        System.out.println("YourNameFilter-->after chain");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
